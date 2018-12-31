250d9a7aa5fdf000ef1f90d3ea458a0b477cf3ad
Jakub Chatrný <jakub.chatrny@gmail.com>
2018-12-31 17:06:06 +0100

    first working version

--- index.js
+++ index.js
@@ -1,55 +1,92 @@
 'use strict'
 
-const remote = require('electron').remote;
+const path = require('path');
+
+const pluginConfig = {
+    headerWidth: "59px",
+    headerLeftOffset: "6px",
+    headerBackground: "transparen", //"#393a3d",
+    iconSize: "12px",
+
+    closeIconPath: path.resolve(__dirname, 'assets/titlebutton-close@2.svg'),
+    closeHoverIconPath: path.resolve(__dirname, 'assets/titlebutton-close-hover@2.svg'),
+    closeActiveIconPath: path.resolve(__dirname, 'assets/titlebutton-close-active@2.svg'),
+
+    minimizeIconPath: path.resolve(__dirname, 'assets/titlebutton-minimize@2.svg'),
+    minimizeHoverIconPath: path.resolve(__dirname, 'assets/titlebutton-minimize-hover@2.svg'),
+    minimizeActiveIconPath: path.resolve(__dirname, 'assets/titlebutton-minimize-active@2.svg'),
+
+    maximizeIconPath: path.resolve(__dirname, 'assets/titlebutton-maximize@2.svg'),
+    maximizeHoverIconPath: path.resolve(__dirname, 'assets/titlebutton-maximize-hover@2.svg'),
+    maximizeActiveIconPath: path.resolve(__dirname, 'assets/titlebutton-maximize-active@2.svg'),
+}
 
 exports.decorateConfig = (config) => {
     return Object.assign({}, config, {
         css: `
             .header_windowControls {
-                width: 59px;
-                left: 6px;
+                width: ${pluginConfig.headerWidth};
+                left: ${pluginConfig.headerLeftOffset};
             }
             .header_windowHeader {
-                background: #393a3d;
+                background: ${pluginConfig.headerBackground};
                 border: none;
             }
             .header_closeWindow, .header_minimizeWindowLeft, .header_maximizeWindowLeft {
+                display: flex;
+                justify-content: center;
+                align-items: center;
                 position: relative;
                 width: 20px;
             }
             .header_closeWindow:after, .header_maximizeWindowLeft:after, .header_minimizeWindowLeft:after {
                 content: "";
                 position:absolute;
-                background-size: 12px;
-                background-repeat: no-repeat;
+                background-size: ${pluginConfig.iconSize};
                 background-position: center;
-                border-radius: 50%;
+                background-repeat: no-repeat;
                 left: 0;
                 right: 0;
                 top: 0;
                 bottom: 0;
                 margin: auto;
-                z-index: -1;
             }
+
             .header_closeWindow:after {
-                background: url(/home/jakub/.themes/McOS-MJV-Dark-v2.0/gtk-3.0/objects-dark/title-buttons/titlebutton-maximize-active@2.svg);
+                background-image: url(${pluginConfig.closeIconPath});
             }
+            .header_closeWindow:hover:after {
+                background-image: url(${pluginConfig.closeHoverIconPath});
+            }
+            .header_closeWindow:active:after {
+                background-image: url(${pluginConfig.closeActiveIconPath});
+            }
+
             .header_minimizeWindowLeft:after {
-                background: url(/home/jakub/.themes/McOS-MJV-Dark-v2.0/gtk-3.0/objects-dark/title-buttons/titlebutton-maximize-active@2.svg);
+                background-image: url(${pluginConfig.minimizeIconPath});
             }
+            .header_minimizeWindowLeft:hover:after {
+                background-image: url(${pluginConfig.minimizeHoverIconPath});
+            }
+            .header_minimizeWindowLeft:active:after {
+                background-image: url(${pluginConfig.minimizeActiveIconPath});
+            }
+
             .header_maximizeWindowLeft:after {
-                background: url(/home/jakub/.themes/McOS-MJV-Dark-v2.0/gtk-3.0/objects-dark/title-buttons/titlebutton-maximize-active@2.svg);
+                background-image: url(${pluginConfig.maximizeIconPath});
+            }
+            .header_maximizeWindowLeft:hover:after {
+                background-image: url(${pluginConfig.maximizeHoverIconPath});
+            }
+            .header_maximizeWindowLeft:active:after {
+                background-image: url(${config.maximizeActiveIconPath});
             }
             
             .header_closeWindow > .header_shape, .header_maximizeWindowLeft > .header_shape, .header_minimizeWindowLeft > .header_shape {
-                displau: none;
-                /*width: 100%;
-                height: 100%;
-                padding: 0;
-                fill: #393a3d;
+                width: ${pluginConfig.iconSize};
+                height: ${pluginConfig.iconSize};
     
-                opacity: 1;
-                transform: scale(0.2);*/
+                opacity: 0;
             }
         `
     })
